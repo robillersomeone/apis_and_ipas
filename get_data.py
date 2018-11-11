4c9fe6defef8e3f4f1c1a88846e999e97ab45066
Jaime Cheng <Clementine@Clementine.local>
2018-11-11 17:33:50 -0500

    add beers and styles to databse

--- get_data.py
+++ get_data.py
@@ -1,15 +1,7 @@
 import requests
-from beer_and_styles import Beer, Style
+from beer_and_styles import Beer, Style, Base
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-engine = sqlalchemy.create_engine('sqlite:///beers.db', echo=True)
-Base = declarative_base()
-Base.metadata.create_all(engine)
-
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
 
 
 class GetBeerData:
@@ -31,31 +23,55 @@ class BeerBuilder:
         beers = []
         for beer in beer_data.get_beer_data():
             try:
-                each_beer = Beer(name = beer['name'],
-                nameDisplay = beer['nameDisplay'], description = beer['description'],
-                abv = beer['abv'], ibu = beer['ibu'], isOrganic = beer['isOrganic'],
-                foodPairings = beer['foodPairings'], style = beer['style']['name'])
-                Beer.style_shortname = session.query(Style).filter(Style.style_shortName == beer['style']['shortName'])
+                beer['style']
+                try:
+                    beer['style']['shortName']
+                    try:
+                        #everything
+                        each_beer = Beer(name = beer['name'],
+                        nameDisplay = beer['nameDisplay'], description = beer['description'],
+                        abv = beer['abv'], ibu = beer['ibu'], isOrganic = beer['isOrganic'],
+                        foodPairings = beer['foodPairings'], style = beer['style']['name'])
+                        each_beer.style_shortName = session.query(Style).filter(Style.shortName == beer['style']['shortName'])
+
+                    except:
+                        each_beer = Beer(name = beer['name'],
+                        nameDisplay = beer['nameDisplay'], isOrganic = beer['isOrganic'],
+                        style = beer['style']['name'])
+                        each_beer.style_shortName = session.query(Style).filter(Style.shortName == beer['style']['shortName'])
+                except:
+                    pass
             except:
-                each_beer = Beer(name = beer['name'],
-                nameDisplay = beer['nameDisplay'], abv = beer['abv'], isOrganic = beer['isOrganic'],
-                style = beer['style']['name'])
-                Beer.style_shortname = session.query(Style).filter(Style.style_shortName == beer['style']['shortName'])
+                pass
             beers.append(each_beer)
         return beers
-
 
 
 class StyleBuilder:
     def run(self):
         styler = requests.get('http://api.brewerydb.com/v2/styles?key=f684318419e2e8da8014c50bb077ddc6&p=1')
         style_data = styler.json()['data']
-        styles = []
+        beer_styles = []
         for style in style_data:
             try:
                 each_style = Style(name = style['name'], category = style['category']['name'], description = style['description'], ibuMin = style['ibuMin'], ibuMax = style['ibuMax'], abvMin = style['abvMin'], abvMax = style['abvMax'])
             except:
                 each_style = Style(name = style['name'], category = style['category']['name'])
-            styles.append(each_style)
-            session.add_all(styles)
-        return styles
+            beer_styles.append(each_style)
+        return beer_styles
+
+
+engine = sqlalchemy.create_engine('sqlite:///beers.db', echo=True)
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+
+x = StyleBuilder()
+session.add_all(x.run())
+session.commit()
+
+y = BeerBuilder()
+session.add_all(y.run())
+session.commit()
