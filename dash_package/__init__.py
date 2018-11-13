027b8bfb3184f24697c71818daad3a601c588ba5
Jaime Cheng <Clementine@Clementine.local>
2018-11-13 15:31:39 -0500

    all new files in Flask SqlAlchemy

---
+++ dash_package/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import dash
+
+server = Flask(__name__)
+server.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///beers.db'
+server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+server.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(server)
+
+app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
+
+from dash_package.get_data import GetBeerData, BeerBuilder, StyleBuilder
+
+from dash_package.models import *
+from dash_package.routes import *
+from dash_package.plots import *
+from dash_package.dashboard import *
