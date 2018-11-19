31cb68f36d4ce4ccb9b508438211b9b1feec5c03
Jaime Cheng <Clementine@Clementine.local>
2018-11-18 21:02:45 -0500

    three tabs and css styling, no box plot yet

--- dash_package/dashboard.py
+++ dash_package/dashboard.py
@@ -9,6 +9,12 @@ import plotly.graph_objs as go
 import sqlalchemy
 
 
+y0 = abv_box('Saison')
+style = go.Box(
+    y = y0
+)
+data = [style]
+
 app.layout = html.Div([
 
     html.H1("Learn more about your favorite beers!"),
@@ -20,34 +26,41 @@ app.layout = html.Div([
         ),
     html.P(style_description("Saison"), id = 'style-description'),
     dcc.Tabs(id="tabs", children=[
-        dcc.Tab(label='Top Descripors', children=[
+        dcc.Tab(label='Descriptive Words', children=[
             html.Div([
+                html.H5(count_beers_in_style("Saison"), id = "beers_analyzed"),
                 dcc.Graph(
                     id='beer-histogram',
                     figure={
-                        'data': [plot_words('Saison')]
+                        'data': [plot_words('Saison')],
+                        'marker':{'color':'rgb(101, 32, 31)'}
                     }
                 )
             ])
         ]),
         dcc.Tab(label='Food Pairings', children=[
-                html.H3(style_name("Saison"), id = "style"),
-                html.H5(style_foodpairings('Saison'), id = "foodpairings")
+                html.H4(style_name("Saison"), id = "style"),
+                html.H6(style_foodpairings('Saison'), id = "foodpairings")
         ]),
         dcc.Tab(label='ABV vs. IBU', children=[
                 dcc.Graph(
-                    id='example-graph-2',
+                    id='abv-ibu',
                     figure={
-                        'data': [
-                            {'x': [1, 2, 3], 'y': [2, 4, 3],
-                                'type': 'bar', 'name': 'SF'},
-                            {'x': [1, 2, 3], 'y': [5, 4, 3],
-                             'type': 'bar', 'name': u'Montréal'},
-                        ]
-                    }
-                )
+                        'data': [abv_ibu('Saison')],
+                        'layout':{'title':[min_max_abv('Saison')], # min_max_abv('Saison'), avg_abv('Saison')
+                            'xaxis':{
+                                'title':'Alcohol Content by Volume'
+                                },
+                            'yaxis':{
+                                'title':'International Bitterness Units'
+                                }
+                            }
+                        }
+                    )]
+
+                ),
         ]),
-    ])
+
 ])
 
 
@@ -59,6 +72,12 @@ def update_figure(selected_style):
             'data': [plot_words(selected_style)],
             'layout' : {'title': f'Out of all the beers labeled "{selected_style}", here are the top words in their descriptions:'}
         }
+
+@app.callback(
+dash.dependencies.Output('beers_analyzed', 'children'),
+[dash.dependencies.Input('dropdown', 'value')])
+def update_description(selected_style):
+    return count_beers_in_style(selected_style)
 
 @app.callback(
 dash.dependencies.Output('style-description', 'children'),
@@ -76,9 +95,27 @@ def update_description(selected_style):
 dash.dependencies.Output('foodpairings', 'children'),
 [dash.dependencies.Input('dropdown', 'value')])
 def update_description(selected_style):
-    return style_foodpairings(selected_style)    
+    return style_foodpairings(selected_style)
 
-#
+@app.callback(
+    dash.dependencies.Output('abv-ibu', 'figure'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def update_figure_abv(selected_style):
+    abvs = min_max_abv(selected_style)
+    return {
+            'data':[abv_ibu(selected_style)], # [abv_ibu(selected_style)],avg_abv(selected_style)
+            'layout' : {'title': abvs,
+                        'xaxis':{
+                            'title':'Alcohol Content by Volume'
+                            },
+                        'yaxis':{
+                            'title':'International Bitterness Units'
+                            }
+                        }
+        }
+
+
+
 # @app.callback(
 # dash.dependencies.Output('abv_range', 'children'),
 # [dash.dependencies.Input('dropdown', 'value')])
